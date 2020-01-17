c71a53c105df1e316fac0df40637b89660283438
Angel Mariya Paul <ot@192-168-1-103.tpgi.com.au>
2020-01-17 17:49:13 +1100

    MySQL configurations

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+//Obtain the credentials for mysql
+$user_name = getopt("u:");
+$host = getopt("h:");
+$password = getopt("p:");
+
+//validate the input values
+if(empty($user_name)){
+  exit("Please enter MySQL user name \n");
+}
+if(empty($host)){
+  exit("Please enter MySQL host \n");
+}
+
+$user_name = $user_name['u'];
+$host = $host['h'];
+
+//sometimes on local machine mysql is set to not have password. So following line is to include that condition
+$password = (empty($password))?'':$password['p'];
+
+?>
--- user_upload.php
+++ user_upload.php
@@ -1,3 +1,17 @@
 <?php
+include 'config.php';
+
+//Initialize database name
+$db_name = "task1";
+
+//try connecting to MySQL and catch any exception
+try{
+  $conn= new PDO("mysql:host=$host;unix_socket=/tmp/mysql.sock;dbname=$db_name",$user_name,$password);
+}catch(PDOException $e){
+  echo "\n Error:".$e->getMessage();
+  exit("\n Please enter valid MySQL credentials \n");
+}
+
+echo "All good";
 
 ?>
